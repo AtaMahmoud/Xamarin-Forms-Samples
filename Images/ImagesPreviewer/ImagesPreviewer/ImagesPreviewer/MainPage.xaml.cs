06a37bf2b52b2c66bbd3452443de6b92c7ee8b17
Ata Mahmoud <ata.ata250@hotmail.com>
2017-08-12 16:39:46 +0200

    Final app

--- Images/ImagesPreviewer/ImagesPreviewer/ImagesPreviewer/MainPage.xaml.cs
+++ Images/ImagesPreviewer/ImagesPreviewer/ImagesPreviewer/MainPage.xaml.cs
@@ -9,9 +9,45 @@ namespace ImagesPreviewer
 {
     public partial class MainPage : ContentPage
     {
+        public int ImageId { get; set; }
         public MainPage()
         {
             InitializeComponent();
+            ImageId = 0;
+            LoadImage();
+            
+        }
+        
+        void LoadImage()
+        {
+            image.Source = new UriImageSource
+            {
+                Uri = new Uri(string.Format("http://lorempixel.com/1920/1080/city/{0}", ImageId)),
+                CachingEnabled = false
+            };
+              
+        }
+        private void Previous_Clicked(object sender, EventArgs e)
+        {
+            ImageId--;
+            if (ImageId == 0)
+            {
+                ImageId = 10;
+            }
+            LoadImage();
+           
+                
+
+        }
+
+        private void Next_Clicked(object sender, EventArgs e)
+        {
+            ImageId++;
+            if(ImageId ==10)
+            {
+                ImageId = 0;
+            }
+            LoadImage();
         }
     }
 }
--- Images/ImagesPreviewer/ImagesPreviewer/ImagesPreviewer/obj/Debug/ImagesPreviewer.MainPage.xaml.g.cs
+++ Images/ImagesPreviewer/ImagesPreviewer/ImagesPreviewer/obj/Debug/ImagesPreviewer.MainPage.xaml.g.cs
@@ -11,13 +11,25 @@
 namespace ImagesPreviewer {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("G:\\Pro\\Xamarin\\Xamarin Samples\\Images\\ImagesPreviewer\\ImagesPreviewer\\ImagesPrevi" +
-        "ewer\\MainPage.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("G:\\Pro\\Xamarin\\Xamarin-Forms-Samples\\Xamarin-Forms-Samples\\Images\\ImagesPreviewer" +
+        "\\ImagesPreviewer\\ImagesPreviewer\\MainPage.xaml")]
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Image image;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Button Previous;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Button Next;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            image = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Image>(this, "image");
+            Previous = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "Previous");
+            Next = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "Next");
         }
     }
 }
